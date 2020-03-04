dcf92056bd9802eded118f5eb53302290bf90bbe
Nicolas Wicki <nwicki@ethz.ch>
2020-03-04 09:24:38 +0100

    Ignore commit

--- compiler-rt/test/dfsan/control_flow_tests/test_foo_4.cpp
+++ compiler-rt/test/dfsan/control_flow_tests/test_foo_4.cpp
@@ -5,30 +5,30 @@
 
 int foo(int a)
 {
-	int x = 0;
-	if(a)
-	{
-		x = 5;
-	}
-	else
-	{
-		x = 3;
-	}
-	int sum = 0;
-	for (int i = 0; i < x; ++i)
-	{
-		sum += i;
-	}
-	return sum;
+       int x = 0;
+       if(a)
+       {
+               x = 5;
+       }
+       else
+       {
+               x = 3;
+       }
+       int sum = 0;
+       for (int i = 0; i < x; ++i)
+       {
+               sum += i;
+       }
+       return sum;
 }
 
 int main(int argc, char** argv)
 {
-	int a = 1;
-	dfsan_label a_label = dfsan_create_label("a", 0);
-	dfsan_set_label(a_label,&a,sizeof(a));
-	int x = foo(a);
-	dfsan_label x_label = dfsan_get_label(x);
-	assert(dfsan_has_label(a_label,x_label));
-	return 0;
+       int a = 1;
+       dfsan_label a_label = dfsan_create_label("a", 0);
+       dfsan_set_label(a_label,&a,sizeof(a));
+       int x = foo(a);
+       dfsan_label x_label = dfsan_get_label(x);
+       assert(dfsan_has_label(a_label,x_label));
+       return 0;
 }
